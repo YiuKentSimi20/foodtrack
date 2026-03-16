979db84f918e1d11b1fe942260696fe0626c8388
YiuKentSimi20 <alexandru.ilia@student.upt.ro>
2026-03-16 22:11:26 +0200

    Initial commit: Structura pentru licenta

---
+++ backend-spring-boot/src/test/java/com/licenta/foodtrack/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licenta.foodtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
