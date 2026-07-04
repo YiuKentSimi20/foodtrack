2ce635d081db10ea8b54e6962843306537db81dd
YiuKentSimi20 <alexandru.ilia@student.upt.ro>
2026-07-04 17:55:40 +0300

    Adaugat BASE_URL ca si variabila de mediu

--- frontend-flutter/frontend_flutter/lib/core/config.dart
+++ frontend-flutter/frontend_flutter/lib/core/config.dart
@@ -1,6 +1,7 @@
 class AppConfig {
-  // Android emulator: 10.0.2.2; desktop/web: localhost
-  // 10.91.62.150 - hotspot
-  // 192.168.1.6 - wifi acasa
-  static const String baseUrl = 'http://192.168.1.2:8083';
+
+  static const String baseUrl = String.fromEnvironment(
+      'BASE_URL',
+      defaultValue: 'http://10.0.2.2:8083'
+  );
 }
